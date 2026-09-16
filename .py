45e9376f029f87afa3be7a1fe24344b47d368a5a
Theman75 <167499780+Diffuse-sea4@users.noreply.github.com>
2026-09-16 02:09:48 +0000

    Change .py for inputing a vowel or consonant

--- .py
+++ .py
@@ -1,3 +1,11 @@
-val = int(input("Enter Your Number:"))
-val2 = int(input("Enter Your Second Number:"))
-print("Sum:", val, val2)
+letter = input("Letter: ").upper()
+
+if len(letter) == 1 and letter.isalpha():
+    if letter in ["A", "E", "I", "O", "U"]:
+        print(letter, "is a vowel")
+    else:
+        print(letter, "is a consonant")
+else:
+    print("invalid input, please enter a single letter")
+# this may look ai generated but i only do stuff like , and then a space like print(letter, "e") tho its easier to do print(letter,"e") i like doing the other way its just like more... yk
+# alr
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# shiny-octo-pancake
+# My robotics class repository
+**Yea i only have this for my robotics so like i can code in it yk**  
+and yk  
+lol alr  
