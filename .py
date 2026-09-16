a8150495ba75fc5af3b50b59ff4269c325ea66ff
Theman75 <167499780+Diffuse-sea4@users.noreply.github.com>
2026-09-16 02:12:55 +0000

    Just add an empty line line 10

--- .py
+++ .py
@@ -7,5 +7,6 @@ if len(letter) == 1 and letter.isalpha():
         print(letter, "is a consonant")
 else:
     print("invalid input, please enter a single letter")
+
 # this may look ai generated but i only do stuff like , and then a space like print(letter, "e") tho its easier to do print(letter,"e") i like doing the other way its just like more... yk
 # alr
