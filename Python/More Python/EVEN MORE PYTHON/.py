278c259686ba80d8bd0e32c68b127a0ef61986f5
Theman75 <167499780+Diffuse-sea4@users.noreply.github.com>
2026-09-16 02:25:01 +0000

    COMMIT

---
+++ Python/More Python/EVEN MORE PYTHON/.py
@@ -0,0 +1,2 @@
+# write a python program to check if the number entered by the user is an even  no. or not and if it is even further it should be checked whether it is greater than 5 or not
+number = int(input("Enter a number:"))
